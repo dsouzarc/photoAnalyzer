d4d28be494e89834f894a9cee191d28f1a2a3e7e
Ryan D'souza <dsouzarc@gmail.com>
2017-01-03 15:43:26 +0800

    First commit

---
+++ PhotoAnalyzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoAnalyzer
+//
+//  Created by Ryan D'souza on 1/3/17.
+//  Copyright © 2017 Ryan D'souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
