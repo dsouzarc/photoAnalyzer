13dc87bc8be8e3b9eb53c424737f55268bbcbadc
Ryan D'souza <dsouzarc@gmail.com>
2017-01-12 02:30:30 -0500

    Added Realm

--- PhotoAnalyzer/DeleteAllPhotosViewController.swift
+++ PhotoAnalyzer/DeleteAllPhotosViewController.swift
@@ -29,45 +29,9 @@ class DeleteAllPhotosViewController : UIViewController, UITableViewDataSource, U
             
         }
         
-        let fet2 = PHFetchOptions()
-        fet2.includeHiddenAssets = true
-        fet2.includeAllBurstAssets = true
-        //fet2.includeAssetSourceTypes = PHAssetSourceType.typeUserLibrary
-        let fetchResults2 = PHAsset.fetchAssets(with: fet2)
+
         
-        print(fetchResults2)
-        var totalImages = 0
         
-        let photoOptions = PHImageRequestOptions.init()
-        photoOptions.isSynchronous = true
-        photoOptions.deliveryMode = PHImageRequestOptionsDeliveryMode.highQualityFormat
-        photoOptions.isNetworkAccessAllowed = true
-        photoOptions.resizeMode = PHImageRequestOptionsResizeMode.exact
-        photoOptions.version = PHImageRequestOptionsVersion.original
-        
-        var totalMemory: Float = 0.0;
-        /*for fetchIndex in 0 ... fetchResults2.count - 1 {
-            let item: PHAsset = fetchResults2.object(at: fetchIndex)
-            //self.allPhotos.append(item)
-            
-            autoreleasepool {
-                PHImageManager.default().requestImageData(for: item, options: photoOptions, resultHandler: {(data: Data?, identificador: String?, orientaciomImage: UIImageOrientation, info: [AnyHashable: Any]?) -> Void in
-                    
-                    var imageSize = Float(data!.count)
-                
-                    //Transform into Megabytes
-                    imageSize = imageSize/(1024*1024)
-                    totalMemory = imageSize + totalMemory;
-                
-                    print("HERE: Date: \(item.creationDate!) isHidden: \(item.isHidden)\tSIZE: \(imageSize)\tTOTAL: \(totalMemory)\tDICT: \(info)")
-                
-                    self.progressView?.setProgress(Float(totalImages / fetchResults2.count), animated: true)
-                })
-            
-                totalImages += 1
-            }
-        }*/
-        print("Total: \(totalImages)")
     }
 
     
